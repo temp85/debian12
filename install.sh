ed3c29d7054b1040c50ad62e6dbc5ade3ffe54a5
temp85 <77952287+temp85@users.noreply.github.com>
2023-10-30 16:58:59 +0700

    123

--- install.sh
+++ install.sh
@@ -1,49 +1,17 @@
-#/bin/bash
-sudo apt install git python3-apt python3-debian pandoc apt-transport-https curl git preload -y
-
-echo "[-] Install [-]"
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-echo 'deb http://download.opensuse.org/repositories/home:/lamlng/Debian_Unstable/ /' | sudo tee /etc/apt/sources.list.d/home:lamlng.list
-curl -fsSL https://download.opensuse.org/repositories/home:lamlng/Debian_Unstable/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_lamlng.gpg > /dev/null
-sudo apt update 
-sudo apt install nala brave-browser ibus ibus-bamboo --install-recommends -y
-ibus restart
-env DCONF_PROFILE=ibus dconf write /desktop/ibus/general/preload-engines "['BambooUs', 'Bamboo']" && gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'us'), ('ibus', 'Bamboo')]"
-
-sudo apt install gnome-themes-extra gtk2-engines-murrine sassc -y
-
-
-mkdir ~/.fonts
-cd /home/temp/Downloads
-git clone --depth 1 https://github.com/ryanoasis/nerd-fonts.git 
-cd nerd-fonts/
-./install.sh
-cd ..
-rm -rf nerd-fonts
-
-cd ~/Downloads
-git clone https://github.com/vinceliuice/Orchis-theme.git
-
-cd Orchis-theme/
-./install.sh -l  --tweaks macos
-sudo apt install ostree appstream-util flatpak -y
-flatpak override --user --filesystem=xdg-config/gtk-4.0
-sudo eflatpak override --filesystem=xdg-config/gtk-4.0
-cp -r ~/.themes/Orchis-Dark-Compact/gtk-4.0/* ~/.config/gtk-4.0/
-
-cd ~/Downloads/
-git clone https://github.com/vinceliuice/Tela-icon-theme.git
-cd Tela-icon-theme/
-./install.sh
-cd ..
-rm -rf Tela-icon-theme
-
-cd ~/Downloads
-git clone https://github.com/EliverLara/Nordic.git
-mv Nordic ~/.themes/
-
-
-
-
-
+#!/bin/bash
+wget https://dl.thorium.rocks/debian/dists/stable/thorium.list
+sudo mv thorium.list /etc/apt/sources.list.d/
+sudo add-apt-repository ppa:papirus/papirus -y
+sudo apt update
+sudo apt install kwrite htop neofetch git thorium-browser preload qt5-style-kvantum qt5-style-kvantum-themes -y
+cd Downloads/
+ls
+git clone https://github.com/L4ki/Wings-Plasma-Themes.git
+cd Wings-Plasma-Themes/
+cp -r Wings\ Global\ Themes/* ~/.local/share/plasma/look-and-feel/
+cp -R Wings\ Plasma\ Themes/* ~/.local/share/plasma/desktoptheme/
+cp -R Wings\ Window\ Decorations/* ~/.local/share/aurorae/themes/
+cp -R Wings\ Color\ Schemes/* ~/.local/share/color-schemes
+cp -R Wings\ Icons\ Themes/* ~/.local/share/icons/
+cp -R Wings\ Konsole\ Color\ Schemes/* ~/.local/share/konsole/
+sudo cp -R ~/Downloads/Wings-Plasma-Themes/Wings\ SDDM\ Themes/* /usr/share/sddm/themes/
